cb4af5ffa2c49f5bc403b2ef03ad56afb8d91e6e
AbdelhadiBouali <ha_bouali@esi.dz>
2019-11-23 15:43:49 +0100

    implementing the frontend

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.frontend')
+
+@section('title')
+    Home
+@endsection
+
+
+@section('content')hello
+@endsection
+
+
+@section('scripts')
+
+@endsection
---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>
+    @yield('title') | Projet IGL
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('frontend/css/bootstrap.css')}}" />
+    
+</head>
+<body>
+    @include('layouts.inc.navbar')
+    <div class="content">
+         @yield('content')
+    </div>
+    
+    <script src="{{asset('frontend/js/jquery-3.4.1.js')}}"></script>
+    <script src="{{asset('frontend/js/popper.min.js')}}"></script>
+    <script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+    @yield('scripts')
+</body>
+</html>
